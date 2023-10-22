9232789a4da931effdd9e1bafcc244f6f26c3733
andreialexandru02 <93448105+andreialexandru02@users.noreply.github.com>
2023-10-22 22:25:08 +0300

    Add files via upload

---
+++ RGB_LED_Potentionemters/RGB_LED_Potentionemters.ino
@@ -0,0 +1,47 @@
+ const int RedInputPin = A0;
+const int GreenInputPin = A1;
+const int BlueInputPin = A2;
+
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+int mappedRedValue;   
+int mappedBlueValue;   
+int mappedGreenValue;   
+
+
+
+const int blueLedPin = 9;
+const int redLedPin = 11;
+const int greenLedPin = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+
+}
+
+void loop() {
+    redValue = analogRead(RedInputPin);
+    greenValue = analogRead(GreenInputPin);
+    blueValue = analogRead(BlueInputPin);
+
+    mappedRedValue = map(redValue, 0, 1023, 0, 255);
+    mappedBlueValue = map(blueValue, 0, 1023, 0, 255);
+    mappedGreenValue = map(greenValue, 0, 1023, 0, 255);
+
+
+    Serial.println(mappedGreenValue);
+    Serial.println(mappedRedValue);
+    Serial.println(mappedBlueValue);
+    //Serial.println(blueValue);
+    //Serial.println(greenValue);
+    analogWrite(redLedPin,mappedRedValue);
+    analogWrite(greenLedPin,mappedGreenValue);
+    analogWrite(blueLedPin,mappedBlueValue);
+    delay(100);
+    
+}
